e295b31daa7dd700330a1798a4e6936b8fa5aec2
zj565061763 <565061763@qq.com>
2023-03-07 12:15:29 +0800

    kotlin

--- lib/src/main/java/com/sd/lib/umeng/common/LibUmengCommon.kt
+++ lib/src/main/java/com/sd/lib/umeng/common/LibUmengCommon.kt
@@ -1,67 +1,45 @@
-package com.sd.lib.umeng.common;
+package com.sd.lib.umeng.common
 
-import android.content.Context;
-import android.text.TextUtils;
+import android.content.Context
+import com.umeng.commonsdk.UMConfigure
 
-import com.umeng.analytics.MobclickAgent;
-import com.umeng.commonsdk.UMConfigure;
-
-public class LibUmengCommon {
-    private static String sAppKey = "";
-    private static String sChannel = "";
-
-    private LibUmengCommon() {
-    }
-
-    /**
-     * {@link #preInit(Context, String, String)}
-     */
-    public static void preInit(Context context, String appKey) {
-        preInit(context, appKey, "umeng");
-    }
+object LibUmengCommon {
+    private var _appKey = ""
+    private var _channel = ""
 
     /**
      * 预初始化
      */
-    public static void preInit(Context context, String appKey, String channel) {
-        if (TextUtils.isEmpty(appKey)) {
-            throw new IllegalArgumentException("appKey is empty");
-        }
-        if (TextUtils.isEmpty(channel)) {
-            throw new IllegalArgumentException("channel is empty");
-        }
-
-        sAppKey = appKey;
-        sChannel = channel;
-
-        UMConfigure.preInit(context, appKey, channel);
-    }
-
-    /**
-     * {@link #init(Context, String, int)}
-     */
-    public static void init(Context context, String pushMessageSecret) {
-        init(context, pushMessageSecret, UMConfigure.DEVICE_TYPE_PHONE);
+    @JvmOverloads
+    fun preInit(
+        context: Context,
+        appKey: String,
+        channel: String = "umeng",
+    ) {
+        require(appKey.isNotEmpty()) { "appKey is empty" }
+        require(channel.isNotEmpty()) { "channel is empty" }
+        _appKey = appKey
+        _channel = channel
+        UMConfigure.preInit(context, appKey, channel)
     }
 
     /**
      * 初始化
      *
      * @param pushMessageSecret 推送MessageSecret
-     * @param type              {@link UMConfigure#DEVICE_TYPE_PHONE}
      */
-    public static void init(Context context, String pushMessageSecret, int type) {
-        final String appKey = sAppKey;
-        if (TextUtils.isEmpty(appKey)) {
-            throw new IllegalStateException("You should call preInit before this");
-        }
+    @JvmOverloads
+    fun init(
+        context: Context,
+        pushMessageSecret: String = "",
+        type: Int = UMConfigure.DEVICE_TYPE_PHONE,
+    ) {
+        val appKey = _appKey
+        check(appKey.isNotEmpty()) { "You should call preInit before this" }
 
-        final String channel = sChannel;
-        if (TextUtils.isEmpty(channel)) {
-            throw new IllegalStateException("You should call preInit before this");
-        }
+        val channel = _channel
+        check(channel.isNotEmpty()) { "You should call preInit before this" }
 
-        UMConfigure.init(context, appKey, channel, type, pushMessageSecret);
-        MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO);
+        UMConfigure.init(context, appKey, channel, type, pushMessageSecret)
     }
 }
